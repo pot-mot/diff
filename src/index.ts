6b181d34027873afff3b1cc7e25e60bee9bda855
Potmot <377029227@qq.com>
2026-03-17 18:15:03 +0800

    UPDATE: export type in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+export type {
+    CircularReferenceDiff,
+
+    ObjectDiff,
+    PropertyAddedDiffItem,
+    PropertyDeletedDiffItem,
+    PropertyUpdatedDiffItem,
+
+    ArrayDiff,
+    ArrayAddedDiffItem,
+    ArrayUpdatedDiffItem,
+    ArrayDeletedDiffItem,
+    ArrayMovedDiffItem,
+    ArrayEqualsDiffItem,
+} from './type/DiffItem';
+
 export {deepEquals} from './deepEquals';
 export {arrayDiff} from './arrayDiff';
 export {objectDiff} from './objectDiff';
